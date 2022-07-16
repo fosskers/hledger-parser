f31ad433db9911e363722afb6f3dd1f250eb10cf
Colin Woodbury <colin@fosskers.ca>
2022-07-16 14:20:38 -0700

    feat: first pass at `Price::parse`

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,19 @@
 
 #![warn(missing_docs)]
 
+use nom::{
+    character::complete::{alpha1, char, space1},
+    combinator::opt,
+    IResult,
+};
 use time::Date;
 
-/// A block of text can either be a full [`Entry`], or just a comment.
-pub enum EntryOrComment {
+/// A standalone block of text can represent a number of things.
+pub enum Block {
     /// A proper entry.
     Entry(Entry),
+    /// The price of some assets as of a certain date.
+    Price(Price),
     /// Some comment line.
     Comment(String),
 }
@@ -50,6 +57,12 @@ pub struct Value {
     pub currency: Option<String>,
 }
 
+impl Value {
+    fn parse(i: &str) -> IResult<&str, Value> {
+        todo!()
+    }
+}
+
 /// An exchange rate that may be associated with a [`Line`].
 pub enum Exchange {
     /// The cost of exchanging one unit, as in:
@@ -60,4 +73,67 @@ pub enum Exchange {
     ///
     /// > 200000 YEN @@ 1927.20 CAD
     Total(Value),
+}
+
+/// Some updated price of a moving asset.
+///
+/// > P 2022-07-12 TSLA 699.21 U
+pub struct Price {
+    /// The date the new price was recorded.
+    pub date: Date,
+    /// The asset's label.
+    pub asset: String,
+    /// The new value.
+    pub value: Value,
+    /// A possible comment.
+    pub comment: Option<String>,
+}
+
+impl Price {
+    /// Construct a new `Price`.
+    pub fn new<S>(date: Date, asset: S, value: Value, comment: Option<String>) -> Self
+    where
+        S: Into<String>,
+    {
+        Self {
+            date,
+            asset: asset.into(),
+            value,
+            comment,
+        }
+    }
+
+    fn parse(i: &str) -> IResult<&str, Price> {
+        let (i, _) = char('P')(i)?;
+        let (i, _) = space1(i)?;
+        let (i, date) = parse_date(i)?;
+        let (i, _) = space1(i)?;
+        let (i, asset) = alpha1(i)?;
+        let (i, _) = space1(i)?;
+        let (i, value) = Value::parse(i)?;
+        let (i, _) = space1(i)?;
+        let (i, comment) = opt(parse_comment)(i)?;
+
+        let price = Price::new(date, asset, value, comment);
+        Ok((i, price))
+    }
+}
+
+fn parse_date(i: &str) -> IResult<&str, Date> {
+    todo!()
+}
+
+fn parse_comment(i: &str) -> IResult<&str, String> {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn prices() {
+        let price = "P 2022-07-12 TSLA 699.21 U";
+        assert!(Price::parse(price).is_ok());
+    }
 }
